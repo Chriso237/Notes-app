a3210e0b69b5de558c3153d1826d27e0b15e67c5
unknown <Christagnissan75@gmail.com>
2023-11-23 08:11:13 +0000

    Push notes-app

---
+++ app.js
@@ -0,0 +1,39 @@
+const yargs = require('yargs')
+
+//Create command add
+yargs.command({
+    command:'add',
+    describe:'add a new note',
+    handler: function(){
+        console.log('adding a note');
+    }
+})
+
+//Create command remove
+yargs.command({
+    command:'remove',
+    describe:'remove a note',
+    handler: function(){
+        console.log('removing a note')
+    }
+})
+
+//Create command list
+yargs.command({
+    command:'list',
+    describe:'list all notes',
+    handler: function(){
+        console.log('listing your notes')
+    }
+})
+
+//Create command read
+yargs.command({
+    command:'read',
+    describe:'read note individually',
+    handler: function(){
+        console.log('reading note')
+    }
+})
+
+yargs.parse()
