b4d7cd55785e459f8c01a10ce483e9a88951e453
unknown <Christagnissan75@gmail.com>
2023-11-25 08:30:43 +0000

    un autre commit

--- notes.js
+++ notes.js
@@ -4,15 +4,33 @@ const getNotes = function(){
     return 'Your notes....'
 }
 
-const addNotes = function(notes){
-    notes = loadNotes()
+const addNotes = function(title, body){
+    const notes = loadNotes()
+    
+
+    notes.push({
+        title: title,
+        body: body
+    })
+
+    saveNotes(notes)
 
 }
 
-const loadNotes = function(){
-    const buffer = fs.readFileSync('notes.json')
-    const notes =  buffer.toString()
-    const notesJSON = JSON.parse(notes)
+const saveNotes = function(notes){ //save the notes
+    const notesJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', notesJSON)
+}
+
+const loadNotes = function(){ //load the notes
+    try {
+        const buffer = fs.readFileSync('notes.json')
+        const notes =  buffer.toString()
+        return JSON.parse(notes)
+    }catch(err){
+        return []
+    }
+   
 }
 
 
